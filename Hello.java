1f68986a813660d1795f5879771e8ed32514ea4b
jeshmitha <jeshgunuganti@gmail.com>
2020-07-25 22:23:07 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Hello
+{
+public static void main(String avg[])
+{
+System.out.println("hello every one");
+}
+}
---
+++ goodbye.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class goodbye
+{
+public static void main(String avg[])
+{
+System.out.println("goodbye everyone");
+}
+}
